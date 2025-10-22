81e0bb683e43fe6c240abde4d5b0c10a6ccea21b
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-10-21 20:59:00 -0400

    Implement delay in zigzag pattern animation
    Added time module and sleep functionality to create a delay in the zigzag pattern.

--- chapter3_functions/zigzag2.py
+++ chapter3_functions/zigzag2.py
@@ -1,3 +1,5 @@
+import time
+
 c = 0
 stars = "********"
 space = " "
@@ -6,7 +8,9 @@ while True:
     if c < 15:
      print(space *c + stars)
      c += 1
+     time.sleep(0.05)
      if c == 14:
         while c != 0:
          print(space * (c - 1) + stars)
          c -= 1
+         time.sleep(0.05)
