8d165d4df9c5c230853e80343d42caa35951d917
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-11-28 15:29:13 -0500

    Implement Collatz sequence generator
    Removed odd/even check and added Collatz sequence logic.

--- chapter3_functions/collatz_sequence.py
+++ chapter3_functions/collatz_sequence.py
@@ -1,10 +1,8 @@
-import sys
-num = int(input("Enter a number: \n"))
-
-
-if num % 2 ==1:
-    print(num, "is odd")
-else:
-        print(num, "is even")
- 
- 
+number = int(input("Enter a positive integer to generate its Collatz sequence: "))
+while number != 1:
+    if number % 2 == 0:
+        number = number // 2
+        print(number)
+    else:
+        number = 3 * number + 1
+        print(number)
