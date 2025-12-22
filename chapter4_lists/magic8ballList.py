cb438d90f50b1524d9316aee1997bff93a69dcfe
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-22 11:11:16 -0500

    Add magic8ballList.py for fortune telling

---
+++ chapter4_lists/magic8ballList.py
@@ -0,0 +1,20 @@
+import random
+
+messages = ["It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later."]
+
+print("ask a question for your fourtune")
+question = input()
+while True:
+    print(random.choice(messages))
+    break
