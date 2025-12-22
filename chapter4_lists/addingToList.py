7a816cde2283e6a6a56ba161016092c9eb99af50
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-22 11:02:57 -0500

    Add interactive list management in addingToList.py

---
+++ chapter4_lists/addingToList.py
@@ -0,0 +1,6 @@
+listOfThings = ["cat", "apple", "banana", "dog", "elephant"]
+print("enter a word to add to the list: ")
+while True:
+ listOfThings.append(input())
+ listOfThings.sort()
+ print(listOfThings)
