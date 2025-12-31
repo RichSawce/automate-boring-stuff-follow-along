023a0242498f66ab6501e916d10b2746c6fecb13
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-31 14:16:44 -0500

    Refactor commaCode.py for better input management
    Refactor input handling and improve list display logic.

--- chapter4_lists/commaCode.py
+++ chapter4_lists/commaCode.py
@@ -3,26 +3,39 @@ spam = []
 cannedSpam = ', '.join(spam[0:-1])
 print("Welcome to your shopping list!")
 while True:
- inputItem = input("Please enter an item to add to your list (or 'done' to finish): ")
- if inputItem == 'done':
-      print("Goodbye!")
-      break
- if inputItem == '':
-        print("Your list is empty, please enter an item")
- else:
-  inputItem != ''
-  spam.append(inputItem)
-  print("would you like to add anything else to your list?")
-  if input().lower == 'no':
-      if spam[:] == 1:
-         print("Have a nice day\n Your list contains:\n", spam[0])
-         sys.exit()
-      else:
-       print("Have a nice day\n Your list contains:\n", cannedSpam, 'and', spam[-1])
+    inputItem = input(
+        "Please enter an item to add to your list (or 'done' to finish): ")
+    if inputItem == 'done':
+        print("Goodbye!")
+        break
+    if inputItem == "":
+        print("You must enter an item")
+
+    else:
+
+        inputItem != "" and 'done'
+        spam.append(inputItem)
+        while True:
+            inputItem = input(
+                "would you like to add anything else to your list?\n")
+
+            if inputItem.lower() == 'no' and spam[0:]:
+                print("Have a nice day1\n Your list contains:\n", *(spam))
+                sys.exit()
 
 
-  if input().lower == 'yes' and spam[0]:
-        print("So far your list contains:\n", *(spam[0:-1]))
-  else:
-        input().lower == 'yes'
-        print("So far your list contains:\n", *(', '.join(spam[0:-1]), 'and', spam[-1]))
+            if inputItem.lower() == 'no' and spam[0:]:
+                print("Have a nice day2\n Your list contains:\n", *
+                      (', '.join(spam[0:-1]), 'and', spam[-1]))
+                sys.exit()
+
+
+            if inputItem.lower() == 'yes' and spam[0]:
+                print("So far your list contains:\n", *(spam))
+                break
+
+            else:
+                inputItem.lower() == 'yes' and spam[0:]
+                print("So far your list contains:\n", *
+                      (', '.join(spam[0:-1]), 'and', spam[-1]))
+                break
