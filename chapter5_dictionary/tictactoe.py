79ff32fa9d805f4f6ede4ae04de4f055b559d616
RichSawce <132316567+RichSawce@users.noreply.github.com>
2026-03-28 15:27:21 -0400

    Enhance Tic Tac Toe input and win logic
    Updated input prompt for player moves and added win condition check.

--- chapter5_dictionary/tictactoe.py
+++ chapter5_dictionary/tictactoe.py
@@ -1,8 +1,18 @@
 #Tic Tac Toe Game
 
+
 board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+win = [['top-L', 'top-M', 'top-R'],
+       ['mid-L', 'mid-M', 'mid-R'],
+       ['low-L', 'low-M', 'low-R'],
+       ['top-L', 'mid-L', 'low-L'],
+       ['top-M', 'mid-M', 'low-M'],
+       ['top-R', 'mid-R', 'low-R'],
+       ['top-L', 'mid-M', 'low-R'],
+       ['top-R', 'mid-M', 'low-L']]
 
 def printBoard():
  print(board['top-L']+ '|' + board['top-M'] + '|' + board['top-R'])
@@ -15,12 +25,13 @@ printBoard()
 
 
 while True:
- move = input("Place your X \n")
+ move = input("Enter the position you want to place your X \n(top-L, top-M, top-R, mid-L, mid-M, mid-R, low-L, low-M, low-R) or type 'quit' to exit: \n")
  if move in board:
    board[move] = 'X'
-   printBoard()
- elif move == '':
-  print("Must enter X\n")
- elif move == 'quit':
-  break
-  
+   printBoard() 
+ if board[move] == win:
+    print("Congratulations! You win!")
+ if move not in board and move != 'quit':
+    print("Must enter a vaild position\n")
+ if move == 'quit':
+   break
