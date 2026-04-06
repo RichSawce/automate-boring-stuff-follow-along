fa5717eaa9f291147ee68c282c50e7d4d0239422
RichSawce <132316567+RichSawce@users.noreply.github.com>
2026-04-06 16:27:41 -0400

    Implement addToInventory function and update display
    Added a function to add loot items to the inventory and updated the display logic.

--- chapter5_dictionary/displayInventory.py
+++ chapter5_dictionary/displayInventory.py
@@ -1,5 +1,7 @@
 inventory = {"sword": 1, "shield": 1, "potion": 5, "gold": 100, "armor": 1}
 
+loot = ["sword", "potion", "gold", "gold", "gem", "gem", "gem"]
+
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -8,7 +10,14 @@ def displayInventory(inventory):
         item_total = sum(inventory.values())
     print("Total number of items: " + str(item_total))
 
+def addToInventory(inventory, addedItems):
+    addedItems == loot.count(inventory)
+    for k in loot:
+        inventory[k] = inventory.get(k, 0) + 1
+    return inventory
+        
+        
 
-     
-
-displayInventory(inventory)  
+      
+inventory = addToInventory(inventory, loot)  
+displayInventory(inventory)
