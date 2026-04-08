5af0815c0943a6b96df1238f3e8f1302b6a378b0
RichSawce <132316567+RichSawce@users.noreply.github.com>
2026-04-08 14:15:30 -0400

    Add arrayFlip.py to flip a grid and print columns
    Implement array flipping and print each column.

---
+++ chapter4_lists/arrayFlip.py
@@ -0,0 +1,19 @@
+grid = [['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']]
+
+flipped = grid[::-1]
+
+for i in flipped:
+    i = [row[0] for row in flipped[:]]
+    for j in flipped:
+        j = [row[1] for row in flipped[:]]
+        break
+    
+    for k in flipped:
+            k = [row[2] for row in flipped[:]]
+            break
+              
+print(*i)
+print(*j)
+print(*k)
