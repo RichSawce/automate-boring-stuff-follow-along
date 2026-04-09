6f0d44ae42ead11024d3c6f360fbc7e8327bc6f8
RichSawce <132316567+RichSawce@users.noreply.github.com>
2026-04-09 18:34:04 -0400

    Update receipt formatting and add more items

--- chapter6_manipulating_strings/receiptFormatter.py
+++ chapter6_manipulating_strings/receiptFormatter.py
@@ -1,17 +1,17 @@
 def receiptFormat(items, left, right):
-    print('WELCOME TO CORNER SHOP')
-    print('-'*22)
+    print('WELCOME TO CORNER SHOP'.center(28, ' '))
+    print('-'*28)
     for k, v in items.items():
+        k = k.upper()
+        v = "{:.2f}".format(v)
         print(k.ljust(left, ' ')+ str(v).rjust(right, ' '))
 
 def total():
-    print('-'*22)
-    print('TOTAL'.ljust(15, ' ')+ str(result).rjust(5, ' '))
+    print('-'*28)
+    print('TOTAL'.ljust(20, ' ')+ str(result).rjust(8, ' '))
+    print('THANK YOU FOR SHOPPING'.center(28, ' '))
     
-    
-
-
-shoppingList = {'apples': 3.50, 'bread':2.99, 'milk':4.25, 'eggs': 5.00}
+shoppingList = {'apples': 3.50, 'bread':2.99, 'milk':4.25, 'eggs': 5.00, 'peanut butter': 6.75, 'chocolate': 3.25, 'broccoli': 4.50, 'potatoes': 1.99, 'chips': 2.50, 'garlic': 1.25  }
 result = sum(shoppingList.values())
-receiptFormat(shoppingList, 15, 5)
+receiptFormat(shoppingList, 20, 8)
 total()
